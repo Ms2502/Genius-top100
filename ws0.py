abf836207a250eb49f449842b8519c7ee379f830
Ms2502 <maty.salinas@hotmail.com>
2022-06-28 12:39:48 -0300

    parte1 funciona

--- ws0.py
+++ ws0.py
@@ -1,3 +1,4 @@
+from this import s
 import time
 import os.path
 from selenium import webdriver
@@ -26,23 +27,93 @@ print(driver.title)
 print("ok")
 print("")
 
-# loadmore = driver.find_element(By.XPATH,"/html/body/div/div/div[5]/div[2]/div/div[4]/div")
-#loadmore = driver.find_element(By.CLASS_NAME,"SquareButton-sc-109lda7-0.gsoAZX")
-
-maxclick = 9   #9
+#En esta parte tocamos el botón de "Load More" 10 veces, si ocurre algun error se saltea esa parte y sigue adelante
 count = 1
+try:
+    #1
+    loadmore = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH,"//*[@id='top-songs']/div/div[4]/div"))
+    )
+    print("pág.",count)
+    print(loadmore.text)
+    loadmore.click()
+    count += 1
+    #2
+    loadmore = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH,"//*[@id='top-songs']/div/div[4]/div"))
+    )
+    print("pág.",count)
+    print(loadmore.text)
+    loadmore.click()
+    count += 1
+    #3
+    loadmore = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH,"//*[@id='top-songs']/div/div[4]/div"))
+    )
+    print("pág.",count)
+    print(loadmore.text)
+    loadmore.click()
+    count += 1
+    #4
+    loadmore = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH,"//*[@id='top-songs']/div/div[4]/div"))
+    )
+    print("pág.",count)
+    print(loadmore.text)
+    loadmore.click()
+    count += 1
+    #5
+    loadmore = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH,"//*[@id='top-songs']/div/div[4]/div"))
+    )
+    print("pág.",count)
+    print(loadmore.text)
+    loadmore.click()
+    count += 1
+    #6
+    loadmore = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH,"//*[@id='top-songs']/div/div[4]/div"))
+    )
+    print("pág.",count)
+    print(loadmore.text)
+    loadmore.click()
+    count += 1
+    #7
+    loadmore = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH,"//*[@id='top-songs']/div/div[4]/div"))
+    )
+    print("pág.",count)
+    print(loadmore.text)
+    loadmore.click()
+    count += 1
+    #8
+    loadmore = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH,"//*[@id='top-songs']/div/div[4]/div"))
+    )
+    print("pág.",count)
+    print(loadmore.text)
+    loadmore.click()
+    count += 1
+    #9
+    loadmore = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH,"//*[@id='top-songs']/div/div[4]/div"))
+    )
+    print("pág.",count)
+    print(loadmore.text)
+    loadmore.click()
+    count += 1
+    #10
+    loadmore = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH,"//*[@id='top-songs']/div/div[4]/div"))
+    )
+    print("pág.",count)
+    print(loadmore.text)
+    loadmore.click()
+    count += 1
+except:
+    print("excepteado lince")
+    pass
 
-# while count <= maxclick:
-#     print(loadmore.text)
-#     # loadmore.click()
-#     count += 1
-#     print("pag.",count)
-#     time.sleep(3)
-
-
-loadmore = driver.find_element(By.XPATH,"//*[@id='top-songs']/div/div[4]/div")
-print(loadmore.text)
-loadmore.click()
 
 tops = driver.find_element(By.ID,"top-songs")
 songtainer = tops.find_element(By.CLASS_NAME,"PageGridFull-idpot7-0.jeWXO")
@@ -51,13 +122,14 @@ elements = songtainer.find_elements(By.TAG_NAME,"a")
 
 songlist1=[]
 for songstr in elements:
-    song = (songstr.text).split("\n")
-    type(song)
-    song.remove("LYRICS")
+    song = (songstr.text).split("\n") #convertimos el string a una lista
+    song.remove("LYRICS") #le sacamos el elemento que contiene "LYRICS"
+    if len(song) > 4:   #algunas canciones tienen un elemento mas que no nos interesa, lo removemos
+        song.pop(3)
     print(song)
     songlist1.append(song)
 
-
+    
 
 
 
